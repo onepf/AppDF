71bfd5cdd72794f48b05a0e6b8036473f3f085fa
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-05 14:31:48 +0400

    added default value to lists + added methods for individual add/remove

--- tools/model/src/main/java/org/onepf/appdf/model/ApkFilesInfo.java
+++ tools/model/src/main/java/org/onepf/appdf/model/ApkFilesInfo.java
@@ -1,12 +1,13 @@
 package org.onepf.appdf.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApkFilesInfo {
 
 	public static class ApkFile {
 		private String fileName;
-		private List<String> exstension;
+		private List<String> exstension = new ArrayList<String>();
 
 		public String getFileName() {
 			return fileName;
@@ -23,9 +24,13 @@ public class ApkFilesInfo {
 		public void setExstension(List<String> exstension) {
 			this.exstension = exstension;
 		}
+		
+		public void addExtension(String exstension){
+			this.exstension.add(exstension);
+		}
 	}
 
-	private List<ApkFile> apkFiles;
+	private List<ApkFile> apkFiles = new ArrayList<ApkFile>();
 
 	public List<ApkFile> getApkFiles() {
 		return apkFiles;
@@ -33,5 +38,13 @@ public class ApkFilesInfo {
 
 	public void setApkFiles(List<ApkFile> apkFiles) {
 		this.apkFiles = apkFiles;
+	}
+	
+	public void addApkFile(ApkFile apkFile){
+		apkFiles.add(apkFile);
+	}
+	
+	public void removeApkFile(ApkFile apkFile){
+		apkFiles.remove(apkFile);
 	}
 }
