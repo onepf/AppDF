0c3272ee36990b9ba4149431183667747871ec23
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-11 01:19:14 +0400

    Added basic parser test

---
+++ tools/parser-lib/parser/src/test/java/org/onepf/appdf/parser/ParserTest.java
@@ -0,0 +1,39 @@
+package org.onepf.appdf.parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.onepf.appdf.model.Application;
+
+/**
+ * Test covers top level functionality of AppdfFileParser class
+ * @author nivanov
+ *
+ */
+public class ParserTest {
+	
+	private File resource;
+	
+	@Before
+	public void initResource() throws URISyntaxException{
+		URL resourceUrl = ParserTest.class.getResource("life.appdf");
+		resource = new File(resourceUrl.toURI());
+	}
+	
+	@Test
+	public void parserCreation() throws IOException{
+		AppdfFileParser parser = new AppdfFileParser(resource);
+	}
+	
+	@Test
+	public void parseDontFail() throws IOException {
+		AppdfFileParser parser = new AppdfFileParser(resource);
+		Application application = parser.parse();
+		Assert.assertNotEquals(null, application);
+	}
+}
