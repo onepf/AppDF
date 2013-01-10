c4e33823c50ee04d59987c9e9e537f7e2210c27d
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-11 01:37:14 +0400

    Passing simple parsing test since returning empty application on every attempt. Validation logic is yet to be done

--- tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/AppdfFileParser.java
+++ tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/AppdfFileParser.java
@@ -2,11 +2,21 @@ package org.onepf.appdf.parser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import org.onepf.appdf.model.Application;
 
+
+
 public class AppdfFileParser {
+	
+	/**
+	 * This file <b>must</b> be present in appdf format
+	 */
+	
+	private static final String ROOT_FILE_NAME = "description.xml";
 	
 	private final ZipFile zipFile;
 	
@@ -19,7 +29,20 @@ public class AppdfFileParser {
 		this(new File(path));
 	}
 	
+	/**
+	 *TODO Do we beed an async version?}
+	 * @return
+	 */
 	public Application parse(){
-		throw new UnsupportedOperationException();
+		Enumeration<? extends ZipEntry> entries = zipFile.entries();
+		Application application = new Application();
+		while ( entries.hasMoreElements()){
+			ZipEntry elem = entries.nextElement();
+			String name = elem.getName();
+			if(ROOT_FILE_NAME.equals(name)){
+				
+			}
+		}
+		return application;		
 	}
 }
