6c25d33fcd8fc0cf11f5e4ebcea93b5e065041f0
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-15 01:53:03 +0400

    CategorizationParser implementation

---
+++ tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/util/XmlUtil.java
@@ -0,0 +1,26 @@
+package org.onepf.appdf.parser.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XmlUtil {
+
+	private XmlUtil() { 
+	}
+	
+	public static List<Node> extractChildElements(Node parent){
+		List<Node> result = new ArrayList<Node>();
+		NodeList childNodes = parent.getChildNodes();
+		for ( int i = 0 ; i < childNodes.getLength() ; i++ ){
+			Node child = childNodes.item(i);
+			if ( child.getNodeType() == Node.ELEMENT_NODE){
+				result.add(child);
+			}
+		}
+		return result;
+	}
+
+}
