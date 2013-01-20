bb810e4eb915045f9d58080d68b8c1693179e359
Vassili Philippov <vassiliphilippov@yandex.ru>
2013-01-20 23:27:35 +0400

    Initial APK parsing and AppDF generating support is added

--- tools/editor/js/js/appdfeditor.js
+++ tools/editor/js/js/appdfeditor.js
@@ -1,240 +1,28 @@
-var allCategories = {
-	"application" : {
-		"personalization" : [
-			"personlalization A",
-			"personlalization B",
-			"personlalization C"
-		],
-		"finance" : [
-			"Investment",
-			"Banks",
-			"Stocks"
-		]
-	},
-	"game" : {
-		"action" : [
-			"",
-			"Starwats",
-			"Dooms"
-		],
-		"strategy" : [
-			"Heroes",
-			"2d strategy"
-		]
-	}    	
-};
+zip.workerScriptsPath = "js/zip/";
 
-var allLanguages = {
-	"ab" : "Abkhaz",
-	"aa" : "Afar",
-	"af" : "Afrikaans",
-	"ak" : "Akan",
-	"sq" : "Albanian",
-	"am" : "Amharic",
-	"ar" : "Arabic",
-	"ar_eg" : "Arabic (Egypt)",
-	"ar_il" : "Arabic (Israel)",
-	"an" : "Aragonese",
-	"hy" : "Armenian",
-	"as" : "Assamese",
-	"av" : "Avaric",
-	"ae" : "Avestan",
-	"ay" : "Aymara",
-	"az" : "Azerbaijani",
-	"bm" : "Bambara",
-	"ba" : "Bashkir",
-	"eu" : "Basque",
-	"be" : "Belarusian",
-	"bn" : "Bengali",
-	"bh" : "Bihari",
-	"bi" : "Bislama",
-	"bs" : "Bosnian",
-	"br" : "Breton",
-	"bg" : "Bulgarian",
-	"my" : "Burmese",
-	"ca" : "Catalan; Valencian",
-	"ch" : "Chamorro",
-	"ce" : "Chechen",
-	"ny" : "Chichewa; Chewa; Nyanja",
-	"zh" : "Chinese",
-	"zh_cn" : "Chinese (PRC)",
-	"zh_tw" : "Chinese (Taiwan)",
-	"cv" : "Chuvash",
-	"kw" : "Cornish",
-	"co" : "Corsican",
-	"cr" : "Cree",
-	"hr" : "Croatian",
-	"cs" : "Czech",
-	"da" : "Danish",
-	"dv" : "Divehi; Dhivehi; Maldivian;",
-	"nl" : "Dutch",
-	"nl_be" : "Dutch (Belgium)",
-	"nl_nl" : "Dutch (Netherlands)",
-	"dz" : "Dzongkha",
-	"en" : "English",
-	"en_us" : "English (US)",
-	"en_au" : "English (Australia)",
-	"en_gb" : "English (Britain)",
-	"en_cz" : "English (Canada)",
-	"en_nz" : "English (New Zealand)",
-	"en_sg" : "English (Singapore)",
-	"en_cd" : "English (Canada)",
-	"eo" : "Esperanto",
-	"et" : "Estonian",
-	"ee" : "Ewe",
-	"fo" : "Faroese",
-	"fj" : "Fijian",
-	"fi" : "Finnish",
-	"fr" : "French",
-	"fr_be" : "French (Belgium)",
-	"fr_ca" : "French (Canada)",
-	"fr_fr" : "French (France)",
-	"fr_ch" : "French (Switzerland)",
-	"ff" : "Fula; Fulah; Pulaar; Pular",
-	"gl" : "Galician",
-	"ka" : "Georgian",
-	"de" : "German",
-	"de_at" : "German (Austria)",
-	"de_de" : "German (Germany)",
-	"de_li" : "German (Liechtenstein)",
-	"de_ch" : "German (Switzerland)",
-	"el" : "Greek, Modern",
-	"gn" : "Guaraní",
-	"gu" : "Gujarati",
-	"ht" : "Haitian; Haitian Creole",
-	"ha" : "Hausa",
-	"he" : "Hebrew (modern)",
-	"hz" : "Herero",
-	"hi" : "Hindi",
-	"ho" : "Hiri Motu",
-	"hu" : "Hungarian",
-	"ia" : "Interlingua",
-	"id" : "Indonesian",
-	"ie" : "Interlingue",
-	"ga" : "Irish",
-	"ig" : "Igbo",
-	"ik" : "Inupiaq",
-	"io" : "Ido",
-	"is" : "Icelandic",
-	"it" : "Italian",
-	"it_it" : "Italian (Italy)",
-	"it_ch" : "Italian (Switzerland)",
-	"iu" : "Inuktitut",
-	"ja" : "Japanese",
-	"jv" : "Javanese",
-	"kl" : "Kalaallisut, Greenlandic",
-	"kn" : "Kannada",
-	"kr" : "Kanuri",
-	"ks" : "Kashmiri",
-	"kk" : "Kazakh",
-	"km" : "Khmer",
-	"ki" : "Kikuyu, Gikuyu",
-	"rw" : "Kinyarwanda",
-	"ky" : "Kyrgyz",
-	"kv" : "Komi",
-	"kg" : "Kongo",
-	"ko" : "Korean",
-	"ku" : "Kurdish",
-	"kj" : "Kwanyama, Kuanyama",
-	"la" : "Latin",
-	"lb" : "Luxembourgish, Letzeburgesch",
-	"lg" : "Ganda",
-	"li" : "Limburgish, Limburgan, Limburger",
-	"ln" : "Lingala",
-	"lo" : "Lao",
-	"lt" : "Lithuanian",
-	"lu" : "Luba-Katanga",
-	"lv" : "Latvian",
-	"gv" : "Manx",
-	"mk" : "Macedonian",
-	"mg" : "Malagasy",
-	"ms" : "Malay",
-	"ml" : "Malayalam",
-	"mt" : "Maltese",
-	"mi" : "Māori",
-	"mr" : "Marathi (Marāṭhī)",
-	"mh" : "Marshallese",
-	"mn" : "Mongolian",
-	"na" : "Nauru",
-	"nv" : "Navajo, Navaho",
-	"nb" : "Norwegian Bokmål",
-	"nd" : "North Ndebele",
-	"ne" : "Nepali",
-	"ng" : "Ndonga",
-	"nn" : "Norwegian Nynorsk",
-	"no" : "Norwegian",
-	"ii" : "Nuosu",
-	"nr" : "South Ndebele",
-	"oc" : "Occitan",
-	"oj" : "Ojibwe, Ojibwa",
-	"cu" : "Old Church Slavonic, Church Slavic, Church Slavonic, Old Bulgarian, Old Slavonic",
-	"om" : "Oromo",
-	"or" : "Oriya",
-	"os" : "Ossetian, Ossetic",
-	"pa" : "Panjabi, Punjabi",
-	"pi" : "Pāli",
-	"fa" : "Persian",
-	"pl" : "Polish",
-	"ps" : "Pashto, Pushto",
-	"pt" : "Portuguese",
-	"pt_br" : "Portuguese (Brazil)",
-	"pt_pt" : "Portuguese (Portugal)",
-	"qu" : "Quechua",
-	"rm" : "Romansh",
-	"rn" : "Kirundi",
-	"ro" : "Romanian, Moldavian(Romanian from Republic of Moldova)",
-	"ru" : "Russian",
-	"sa" : "Sanskrit (Saṁskṛta)",
-	"sc" : "Sardinian",
-	"sd" : "Sindhi",
-	"se" : "Northern Sami",
-	"sm" : "Samoan",
-	"sg" : "Sango",
-	"sr" : "Serbian",
-	"gd" : "Scottish Gaelic; Gaelic",
-	"sn" : "Shona",
-	"si" : "Sinhala, Sinhalese",
-	"sk" : "Slovak",
-	"sl" : "Slovene",
-	"so" : "Somali",
-	"st" : "Southern Sotho",
-	"es" : "Spanish; Castilian",
-	"su" : "Sundanese",
-	"sw" : "Swahili",
-	"ss" : "Swati",
-	"sv" : "Swedish",
-	"ta" : "Tamil",
-	"te" : "Telugu",
-	"tg" : "Tajik",
-	"th" : "Thai",
-	"ti" : "Tigrinya",
-	"bo" : "Tibetan Standard, Tibetan, Central",
-	"tk" : "Turkmen",
-	"tl" : "Tagalog",
-	"tn" : "Tswana",
-	"to" : "Tonga (Tonga Islands)",
-	"tr" : "Turkish",
-	"ts" : "Tsonga",
-	"tt" : "Tatar",
-	"tw" : "Twi",
-	"ty" : "Tahitian",
-	"ug" : "Uighur, Uyghur",
-	"uk" : "Ukrainian",
-	"ur" : "Urdu",
-	"uz" : "Uzbek",
-	"ve" : "Venda",
-	"vi" : "Vietnamese",
-	"vo" : "Volapük",
-	"wa" : "Walloon",
-	"cy" : "Welsh",
-	"wo" : "Wolof",
-	"fy" : "Western Frisian",
-	"xh" : "Xhosa",
-	"yi" : "Yiddish",
-	"yo" : "Yoruba",
-	"za" : "Zhuang, Chuang",
-	"zu" : "Zulu"
-};
+var allCategories = {};
+var storeCategories = {};
+var allLanguages = {};
+var allStores = {};
+
+var globalUnigueCounter = 0;
+function getUniqueId() {
+	return globalUnigueCounter++;
+}
+
+$.getJSON("../data/categories.json", function(loadedCategories) {
+$.getJSON("../data/store_categories.json", function(loadedStoreCategories) {
+$.getJSON("../data/languages.json", function(loadedLanguages) {
+$.getJSON("../data/stores.json", function(loadedStores) {
+	allCategories = loadedCategories;
+	storeCategories = loadedStoreCategories;
+	allLanguages = loadedLanguages;
+	allStores = loadedStores;
+	initialFilling();
+})
+})
+})			
+})
 
 $('#myTab a').click(function (e) {
   e.preventDefault();
@@ -243,10 +31,69 @@ $('#myTab a').click(function (e) {
 
 $("#categorization-type").change(function() {
 	fillCategories();
+	fillSubcategories();
+	fillCategoryStoresInfo();
 });
 
 $("#categorization-category").change(function() {
 	fillSubcategories();
+	fillCategoryStoresInfo();
+});
+
+$("#categorization-subcategory").change(function() {
+	fillCategoryStoresInfo();
+});
+
+$("#apk-file").change(function() {
+	var fileInputEntry = document.getElementById("apk-file");
+	var error = ApkParser.parseApkFile(fileInputEntry.files[0], fileInputEntry.value, function(apkData) {
+		$('#pretty-apk-file').val($("#apk-file").val().replace("C:\\fakepath\\", ""));
+		alert("package = " + apkData.package);
+	}, function (error) {
+		fileInputEntry.value = '';
+		alert("Error APK File Parsing: " + error);
+	});
+});
+
+function prettyFileInputClick(id) {
+	$('input[id=' + id + ']').click();
+};
+
+function generateAppDFFile(onend) {
+	var descriptionXML = generateDescriptionFileXML(); 
+	console.log(descriptionXML);
+	return;
+
+	var URL = window.webkitURL || window.mozURL || window.URL;
+	var apkFile = document.getElementById("apk-file");
+
+	zip.createWriter(new zip.BlobWriter(), function(writer) {
+
+		addDescriptionAndFilesToZipWriter(writer, descriptionXML, [apkFile.files], onProgress, function() {
+			writer.close(function(blob) {
+				var url = URL.createObjectURL(blob);
+				onend(url);
+		    });
+		  });
+
+	}, function(error) {
+		alert("error:" + error);
+	});
+};
+
+$("#build-appdf-file").click(function(event) {
+	var downloadLink = document.getElementById("build-appdf-file");
+	if (!downloadLink.download) {
+		generateAppDFFile(function(url) {
+			var clickEvent = document.createEvent("MouseEvent");
+			downloadLink.href = url;
+			downloadLink.download = "test.zip";
+			clickEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			downloadLink.dispatchEvent(clickEvent);
+		});
+		event.preventDefault();
+		return false;
+	}
 });
 
 function fillLanguages(element) {
@@ -276,7 +123,30 @@ function fillLanguages(element) {
 		var s = subcategoryArray[i];
 		subcategories.append($("<option />").val(s).text(s));
 	}
+	if (subcategoryArray.length<=1) {
+		subcategories.closest(".control-group").hide();	
+	} else {
+		subcategories.closest(".control-group").show();			
+	}
  }
+
+function fillCategoryStoresInfo() {
+	var table = $("<table class='table table-striped table-bordered'/>");
+	table.append($("<tr><th>Store</th><th>Category</th></tr>"));
+
+	var selectedType = $("#categorization-type").find(":selected").val();
+	var selectedCategory = $("#categorization-category").find(":selected").val();
+	var selectedSubcategory = $("#categorization-subcategory").find(":selected").val();
+
+	var storeInfo = storeCategories[selectedType][selectedCategory][selectedSubcategory];
+
+	for (store in storeInfo) {
+		table.append($("<tr><td>" + allStores[store] + "</td><td>" + storeInfo[store] + "</td></tr>"));
+	}
+
+	$("#store-categories-info").empty();
+	$("#store-categories-info").append(table);
+}
 
  function addMoreTitles(e) {
  	var parent = $(e).closest(".control-group");
@@ -286,7 +156,7 @@ function fillLanguages(element) {
 			<label class="control-label"  for="description-texts-title-more">Longer title</label> \
 			<div class="controls"> \
 				<div class="input-append"> \
-					<input type="text" id="description-texts-title-more" class="input-xxlarge"> \
+					<input type="text" id="description-texts-title-more-' + getUniqueId() + ' class="input-xxlarge"> \
 					<button class="btn" type="button" onclick="removeControlGroup(this); return false;"><i class="icon-remove"></i></button> \
 				</div> \
 				<p class="help-block">Enter longer title and it will be used by those stores that support longer titles.</p> \
@@ -335,15 +205,54 @@ function removeKeyword(e) {
 	$(e).closest(".keyword-countainer").remove();
 }
 
-
 //Initial filling
-fillLanguages($("#description-attrlanguage"));
-fillCategories();
-fillSubcategories();
+function initialFilling() {
+	fillLanguages($("#description-attrlanguage"));
+	fillCategories();
+	fillSubcategories();
+}
+
+function generateCategorizationXML(xml) {
+	xml.addTag("<categorization>", function() {
+		xml.addTag("<type>", $("#categorization-type").val());
+		xml.addTag("<category>", $("#categorization-category").val());
+		xml.addTag("<subcategory>", $("#categorization-subcategory").val());
+	});
+};
+
+function generateDescriptionXML(xml) {
+	xml.addTag("<description>", function() {
+		xml.addTag("<texts>", function() {
+			xml.addTag("<title>", $("#description-texts-title").val());
+			$("section#description input[id^=description-texts-title-more-]").each(function() {
+				xml.addTag("<title>", $(this).val());
+			});
+		});		
+	});
+};
+
+function generateCustomerSupportXML(xml) {
+	xml.addTag("<customer-support>", function() {
+		xml.addTag("<phone>", $("#customersupport-phone").val());
+		xml.addTag("<email>", $("#customersupport-email").val());
+		xml.addTag("<website>", $("#customersupport-website").val());
+	});
+};
+
+function generateDescriptionFileXML() {
+	var xml = new XMLGenerator();
+	xml.addLine('<?xml version="1.0" encoding="UTF-8"?>');
+	xml.addTag('<application-description-file version="1">', function() {
+		xml.addTag('<application package="ru.yandex.shell">', function() {
+			generateCategorizationXML(xml);
+			generateDescriptionXML(xml);
+			generateCustomerSupportXML(xml);
+		});
+	});
+	return xml.getXmlText();
+};
 
 //Add validations
-//	$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
-
 $("input,textarea,select").jqBootstrapValidation(
     {
         preventSubmit: true,
@@ -360,3 +269,44 @@ $("input,textarea,select").jqBootstrapValidation(
         }
     }
 );
+
+function flatten(array) {
+    var flat = [];
+    for (var i = 0; i < array.length; i++) {
+        var type = Object.prototype.toString.call(array[i]).split(' ').pop().split(']').shift().toLowerCase();
+        if (type) { 
+        	if (/^(array|collection|arguments|object|filelist)$/.test(type)) {
+	        	flat = flat.concat(flatten(array[i])); 
+        	} else {
+    	    	flat = flat.concat(array[i]);         		
+        	}
+        }
+    }
+    return flat;
+};
+
+function addDescriptionAndFilesToZipWriter(zipWriter, descriptionXml, files, onprogress, onend) {
+	var addIndex = 0;
+
+	var flattenedFiles = flatten(files);
+
+	function addNextFile() {
+		var file = flattenedFiles[addIndex];
+		zipWriter.add(file.name, new zip.BlobReader(file), function() {
+			addIndex++;
+			if (addIndex < flattenedFiles.length) {
+				addNextFile();
+			} else {
+				onend();
+			}
+		}, onprogress);
+	};
+
+	zipWriter.add("description.xml", new zip.TextReader(descriptionXml), function() {
+		addNextFile();
+	}, onprogress);
+};
+
+function onProgress(current, total) {
+	console.log("progress total=" + total + ", current=" + current);
+};
