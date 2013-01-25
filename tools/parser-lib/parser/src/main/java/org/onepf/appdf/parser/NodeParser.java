a4dd1693a1efdd417bfd2d75f8bca3b704afe43d
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-26 00:40:56 +0400

    Added basic model parsing interface + basic implementation for parsing texts

---
+++ tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/NodeParser.java
@@ -0,0 +1,8 @@
+package org.onepf.appdf.parser;
+
+import org.onepf.appdf.model.ModelElement;
+import org.w3c.dom.Node;
+
+public interface NodeParser<T extends ModelElement> {
+	void parse(Node node,T application);
+}
