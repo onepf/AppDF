9dddd33f2df1a41c2032007e82d0958c084b956a
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-26 01:14:47 +0400

    formatting fixed

--- tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/ParsingException.java
+++ tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/ParsingException.java
@@ -2,26 +2,26 @@ package org.onepf.appdf.parser;
 
 public class ParsingException extends RuntimeException {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public ParsingException(String message) {
-		super(message);
-	}
+    public ParsingException(String message) {
+        super(message);
+    }
 
-	public ParsingException(Throwable cause) {
-		super(cause);
-	}
+    public ParsingException(Throwable cause) {
+        super(cause);
+    }
 
-	public ParsingException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public ParsingException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-	public ParsingException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
+    public ParsingException(String message, Throwable cause,
+            boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 
 }
