10daf7fc272ccece8434e08dc55759169275849e
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-26 13:09:14 +0400

    added base class for parsers based on enums

---
+++ tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/BaseParser.java
@@ -0,0 +1,45 @@
+package org.onepf.appdf.parser;
+
+import java.util.List;
+
+import org.onepf.appdf.model.ModelElement;
+import org.onepf.appdf.parser.util.XmlUtil;
+import org.w3c.dom.Node;
+
+/**
+ * Base class for parser using enums values as inner parsers
+ * 
+ * @author nivanov
+ * 
+ * @param <T>
+ * @param <E>
+ */
+public abstract class BaseParser<T extends ModelElement, E extends Enum<E> & NodeParser<T>>
+        implements NodeParser<T> {
+
+    private final Class<E> enumClass;
+    private final String enclosingTagName;
+
+    public BaseParser(Class<E> enumClass, String enclosingTagName) {
+        this.enumClass = enumClass;
+        this.enclosingTagName = enclosingTagName;
+
+    }
+    
+
+    @Override
+    public void parse(Node node, T element) {
+        List<Node> childNodes = XmlUtil.extractChildElements(node);
+        for (Node childNode : childNodes) {
+            String tagName = childNode.getNodeName();
+            try {
+                E.valueOf(enumClass, tagName.toUpperCase().replace('-', '_'))
+                        .parse(node, element);
+            } catch (IllegalArgumentException iae) {
+                throw new ParsingException("Unsupported tag:" + tagName
+                        + " inside of " + enclosingTagName);
+            }
+        }
+    }
+
+}
