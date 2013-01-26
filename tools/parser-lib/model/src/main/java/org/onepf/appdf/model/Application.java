93ffb552c9b24b7b415aba4efce91f88740fa191
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-26 12:24:29 +0400

    Application implements ModelElement interface

--- tools/parser-lib/model/src/main/java/org/onepf/appdf/model/Application.java
+++ tools/parser-lib/model/src/main/java/org/onepf/appdf/model/Application.java
@@ -8,7 +8,7 @@ import java.util.List;
  *
  */
 
-public class Application {
+public class Application implements ModelElement {
 	
 	private String packageName;
 	private Categorisation categorisation;
