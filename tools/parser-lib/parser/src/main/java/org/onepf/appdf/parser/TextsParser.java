48cc15a57cf8cdd9faa20e575572cd2396531009
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-26 13:19:39 +0400

    extends BaseParser now

--- tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/TextsParser.java
+++ tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/TextsParser.java
@@ -1,24 +1,11 @@
 package org.onepf.appdf.parser;
 
-import java.util.List;
-
 import org.onepf.appdf.model.Description;
-import org.onepf.appdf.parser.util.XmlUtil;
-import org.w3c.dom.Node;
 
-public final class TextsParser implements NodeParser<Description>{
+public class TextsParser extends BaseParser<Description, TextsTags>{
 	public static final String TAG = "texts";
 	
-	@Override
-	public void parse(Node node, Description description) {
-		List<Node> nodes = XmlUtil.extractChildElements(node);
-		for (Node innerNode : nodes){
-			String tagName = innerNode.getNodeName();
-			try{
-				TextsTags.valueOf(tagName.toUpperCase().replace('-', '_')).parse(innerNode, description);
-			}catch(IllegalArgumentException e){
-				throw new ParsingException("Unexpected tag:" + tagName);
-			}
-		}
-	}
+	public TextsParser(){
+	    super(TextsTags.class,"texts");
+	}	
 }
