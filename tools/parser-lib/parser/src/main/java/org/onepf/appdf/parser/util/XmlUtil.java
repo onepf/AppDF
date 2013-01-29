9e26ac653c96b7d5071cc9e2ddcfdda6838c43c9
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-01-30 01:40:59 +0400

    created more generic helper method

--- tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/util/XmlUtil.java
+++ tools/parser-lib/parser/src/main/java/org/onepf/appdf/parser/util/XmlUtil.java
@@ -33,68 +33,86 @@ import org.w3c.dom.NodeList;
 
 public class XmlUtil {
 
-	public static final String YES = "yes";
+    public static final String YES = "yes";
 
-    private XmlUtil() { 
-	}
-	
-	public static List<Node> extractChildElements(Node parent){
-		List<Node> result = new ArrayList<Node>();
-		NodeList childNodes = parent.getChildNodes();
-		for ( int i = 0 ; i < childNodes.getLength() ; i++ ){
-			Node child = childNodes.item(i);
-			if ( child.getNodeType() == Node.ELEMENT_NODE){
-				result.add(child);
-			}
-		}
-		return result;
-	}
-	
-	
+    private XmlUtil() {
+    }
 
-	
-	public static String getOptionalAttributeValue(NamedNodeMap attrs,String name) {
-	
-	    Node namedItem = attrs.getNamedItem(name);
+    public static List<Node> extractChildElements(Node parent) {
+        List<Node> result = new ArrayList<Node>();
+        NodeList childNodes = parent.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node child = childNodes.item(i);
+            if (child.getNodeType() == Node.ELEMENT_NODE) {
+                result.add(child);
+            }
+        }
+        return result;
+    }
+
+    public static String getOptionalAttributeValue(NamedNodeMap attrs,
+            String name) {
+
+        Node namedItem = attrs.getNamedItem(name);
         if (namedItem == null) {
             return "";
-        }else{
+        } else {
             return namedItem.getTextContent();
         }
-	}
+    }
 
     public static String tagNameToFieldName(String tagName) {
         int minusIndex = tagName.indexOf('-');
-        while ( minusIndex != -1){
+        while (minusIndex != -1) {
             char nextChar = tagName.charAt(minusIndex + 1);
             char upper = Character.toUpperCase(nextChar);
             StringBuilder sb = new StringBuilder(tagName);
-            sb.replace(minusIndex + 1, minusIndex + 2, new String(new char[]{upper}));
+            sb.replace(minusIndex + 1, minusIndex + 2, new String(
+                    new char[] { upper }));
             sb.replace(minusIndex, minusIndex + 1, "");
             tagName = sb.toString();
             minusIndex = tagName.indexOf('-');
         }
         return tagName;
     }
-    
-    public static <T> void mapBooleanChildsToBean(Node node,Class<T> beanClass,T bean)
+
+    @SuppressWarnings("unchecked")
+    public static <T> void mapChildsToBean(Node node, Class<T> beanClass, T bean)
             throws DOMException, ParsingException {
         List<Node> childNodes = extractChildElements(node);
-        for(Node child : childNodes){
+        for (Node child : childNodes) {
             String tagName = child.getNodeName();
             String originalTagName = tagName;
             tagName = tagNameToFieldName(tagName);
             String childValue = child.getTextContent();
             try {
-                PropertyDescriptor pd  = new PropertyDescriptor(tagName, beanClass);
+                PropertyDescriptor pd = new PropertyDescriptor(tagName,
+                        beanClass);
                 Method writeMethod = pd.getWriteMethod();
-                
-                boolean boolValue = YES.equalsIgnoreCase(childValue);
-                writeMethod.invoke(bean, boolValue);
+                Class<?> propertyType = pd.getPropertyType();
+
+                final Object param;
+                if (boolean.class.equals(propertyType)) {//
+                    boolean boolValue = YES.equalsIgnoreCase(childValue);
+                    param = boolValue ? Boolean.TRUE : Boolean.FALSE;
+                } else if (int.class.equals(propertyType)) {
+                    param = Integer.parseInt(child.getTextContent());
+                } else if (String.class.equals(propertyType)) {
+                    param = child.getTextContent();
+                } else if (propertyType.isEnum()) {
+                    param = Enum.valueOf(propertyType.asSubclass(Enum.class),
+                            child.getTextContent().toUpperCase());
+                } else {
+                    throw new ParsingException("Unsupported property type:"
+                            + propertyType);
+                }
+                writeMethod.invoke(bean, param);
             } catch (IntrospectionException e) {
-               throw new ParsingException("Unexpected descriptor:" + originalTagName); 
-            } catch (IllegalArgumentException iae){
-                throw new ParsingException("Illegal descriptor value:" + childValue);
+                throw new ParsingException("Unexpected descriptor:"
+                        + originalTagName);
+            } catch (IllegalArgumentException iae) {
+                throw new ParsingException("Illegal descriptor value:"
+                        + childValue);
             } catch (IllegalAccessException e) {
                 throw new ParsingException(e);
             } catch (InvocationTargetException e) {
@@ -102,18 +120,19 @@ public class XmlUtil {
             }
         }
     }
-    
-    public static List<String> collectNodeValues(Node node,String... acceptedTags) throws ParsingException{
+
+    public static List<String> collectNodeValues(Node node,
+            String... acceptedTags) throws ParsingException {
         List<Node> children = extractChildElements(node);
         Set<String> set = new HashSet<>(Arrays.asList(acceptedTags));
         ArrayList<String> result = new ArrayList<String>();
-        for ( Node child : children  ){
+        for (Node child : children) {
             String tag = child.getNodeName();
-            if ( !set.contains(tag)){
+            if (!set.contains(tag)) {
                 throw new ParsingException("Unsupported tag:" + tag);
             }
             result.add(child.getTextContent());
-        }        
+        }
         return result;
     }
 }
