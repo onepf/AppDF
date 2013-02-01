b4092836785677b89bd67d9fab0d4e5735145cc1
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-02-02 00:50:18 +0400

    Cli project creation and initial addition

---
+++ tools/parser-lib/cli/src/main/java/org/onepf/appdf/cli/Starter.java
@@ -0,0 +1,13 @@
+package org.onepf.appdf.cli;
+
+public class Starter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
