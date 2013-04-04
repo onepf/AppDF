94fed8d311fc2651029297c1732abff535c23f92
Nikolay Ivanov <nik.ivanov@gmail.com>
2013-04-04 19:37:59 +0400

    equals and hashcode added

--- tools/parser-lib/model/src/main/java/org/onepf/appdf/model/Category.java
+++ tools/parser-lib/model/src/main/java/org/onepf/appdf/model/Category.java
@@ -76,6 +76,20 @@ public class Category {
         return id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id.equals(category.id);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
     public List<Category> getSubcategories() {
         return subcategories;
     }
